3cb44df1c3b98dd1ea659e0216282c16a9a750bf
Simon Cozens <simon@cpan.org>
2000-06-17 10:56:28 +0000

    Import of SIMON/Mail-Audit-1.2 from CPAN.
    gitpan-cpan-distribution: Mail-Audit
    gitpan-cpan-version:      1.2
    gitpan-cpan-path:         SIMON/Mail-Audit-1.2.tar.gz
    gitpan-cpan-author:       SIMON
    gitpan-cpan-maturity:     released

--- Audit.pm
+++ Audit.pm
@@ -8,21 +8,49 @@ use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
 use Fcntl ':flock';
 use constant REJECTED => 100;
 use constant DELIVERED => 0;
+my $loglevel=3;
+my $logging =0;
+my $logfile = "/tmp/".getpwuid($>)."-audit.log";
 
-$VERSION = '1.1';
+$VERSION = '1.2';
 
-sub new { bless({ obj => Mail::Internet->new(\*STDIN), @_ }, shift) }
+sub _log {
+    my ($priority, $what) = @_; 
+    if ($logging and $priority <= $loglevel) { print LOG "$what\n"; }
+}
+
+sub new { 
+    my $self = bless({ obj => Mail::Internet->new(\*STDIN), @_ }, shift) 
+    if (exists $self->{loglevel}) { 
+        $logging =1;
+        $loglevel = $self->{loglevel};
+    }
+    if (exists $self->{log}) {
+        $logging = 1;
+        $logfile = $self->{log};
+    }
+    if ($logging) {
+        open LOG, ">>$logfile" or die $!;
+        _log(1,"Logging started at ".scalar localtime);
+        _log(2,"Incoming mail from ".$mail->from);
+        _log(2,"To: ".$mail->to);
+        _log(2,"Subject: ".$mail->subject);
+    }
+    return $self;
+}
 
 sub accept {
 	my $self = shift;
 	my $file = shift || "/var/spool/mail/".getpwuid($>);
+    _log(1,"Accepting");
 	return $self->{accept}->() if exists $self->{accept};
-
+    _log(2,"Accepting to $file");
 	# is it a Maildir?
 	if (-d "$file/tmp" && -d "$file/new") {
 		my $msg_file = "/${\time}.$$.${\hostname}";
 		my $tmp_path = "$file/tmp/$msg_file";
 		my $new_path = "$file/new/$msg_file";
+        _log(3,"Looks like maildir, writing to $new_path");
 
 		# since mutt won't add a lines tag to maildir messages,
 		# we'll add it here
@@ -30,27 +58,42 @@ sub accept {
 			my $body = $self->{obj}->body;
 			my $num_lines = @$body;
 			$self->{obj}->head->add("Lines", $num_lines)
+            _log(4,"Adding Lines: $num_lines header");
 		}
-		open TMP, ">$tmp_path" or die $!;
+		unless (open TMP, ">$tmp_path") {
+            _log(0,"Couldn't open $tmp_path! $!");
+            die $!;
+        }
 		print TMP $self->{obj}->as_mbox_string;
 		close TMP;
 
-		link $tmp_path, $new_path or die $!;
-		unlink $tmp_path;
+		unless (link $tmp_path, $new_path) {
+            _log(0,"Couldn't link $tmp_path to $new_path : $!");
+            die $!;
+        }
+		unlink $tmp_path or _log(1,"Couldn't unlink $tmp_path: $!");
 	} else { # it's an mbox
-		open(FH, ">>$file") or die $!;
-		flock(FH, LOCK_EX);
+		unless (open(FH, ">>$file")) {
+            _log(0,"Couldn't open $file! $!");
+            die $!;
+        }
+		flock(FH, LOCK_EX) 
+            or _log(1,"Couldn't get exclusive lock on $file");
 		print FH $self->{obj}->as_mbox_string;
 		flock(FH, LOCK_UN);
+            or _log(1,"Couldn't unlock on $file");
 		close FH;
 	}
+    _log(2,"Exiting with status ".DELIVERED);
 	exit DELIVERED;
 }
 
 sub reject {
 	my $self=shift;
 	return $self->{reject}->() if exists $self->{reject};
+    _log(1, "Rejecting with reason @_");
 	print STDERR @_;
+    _log(2,"Exiting with status ".REJECTED);
 	exit REJECTED;
 }
 
@@ -58,9 +101,15 @@ sub pipe {
 	my $self = shift;
 	my $file = shift;
 	return $self->{pipe}->() if exists $self->{pipe};
-	open (PIPE, "|$file") or $self->accept();
+    _log(1, "Piping to $file");
+	unless (open (PIPE, "|$file")) {
+        _log(0, "Couldn't open pipe $file: $!")
+        $self->accept();
+    }
 	$self->{obj}->print(\*PIPE);
 	close PIPE;
+    _log(3,"Pipe closed with status $?");
+    _log(2,"Exiting with status ".DELIVERED);
 	exit DELIVERED;
 }
 
@@ -71,23 +120,30 @@ sub subject { $_[0]->{obj}->head->get("Subject") }
 sub bcc { $_[0]->{obj}->head->get("Bcc") }
 sub cc { $_[0]->{obj}->head->get("Cc") }
 sub received { $_[0]->{obj}->head->get("Recieved") }
+sub get { $_[0]->{obj}->head->get($_[1]) }
 sub resend {$_[0]->{obj}->smtpsend(To => $_[1]) }
-sub ignore { exit DELIVERED }
+sub ignore { _log(1,"Ignoring"); exit DELIVERED }
 
+sub myALRM { die "alarm\n" }
 sub rblcheck {
 my ($self, $timeout) = (shift, shift);
+_log(1,"Performing RBL check");
 my @recieved      = $self->received;
 my $rcvcount      = 0;
+$timeout = 10 unless defined $timeout;
 
 # Catch ALRM signals so we can timeout DNS lookups
-sub myALRM { die "alarm\n" }
 $SIG{ALRM} = 'myALRM';
 &myALRM() if 0;              # make -w shut up
 for (@recieved) {
-    my $x = _checkit($rcvCount,$header);
-    return $x if $x;
-    $rcvCount++;  # Any further Received lines won't be the first.
+    my $x = _checkit($rcvcount,$_,$timeout);
+    if ($x) {
+        _log(2, "Check returned $x after ".(1+$rcvcount)." recieved headers");
+        return $x
+    }
+    $rcvcount++;  # Any further Received lines won't be the first.
 }
+_log(2, "Check was fine");
 return '';
 }
 
@@ -98,7 +154,7 @@ sub checkit {
     my $RcvBlackHole  = '2 Received from RBL-registered spam site ';
     my $RlyBlackHole  = '3 Relayed through RBL-registered spam site ';
 
-   my($relay,$rcvd) = @_;
+   my($relay,$rcvd,$timeout) = @_;
    my($IP,@IP) = $rcvd =~ /\[((\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}))\]/;
    my($name,$x);
    # We can't complain if there's no IP address in this Received header.
@@ -171,6 +227,12 @@ keys and with subroutine references to override the methods with those
 names. For example, people using MH as their mail handler will want to
 override C<accept> to reflect the local delivery method of that mailer.
 
+You may also specify C<log => $logfile> to write a debugging log; you
+can set the verbosity of the log with the C<loglevel> key, on a scale of
+1 to 4. If you specify a log level without a log file, logging will be
+written to F</tmp/you-audit.log> where F<you> is replaced by your user
+name.
+
 =back
 
 =head2 METHODS
@@ -200,6 +262,12 @@ bounce.
 This merely ignores the email, dropping it into the bit bucket for
 eternity.
 
+=item C<rblcheck([$timeout])>
+
+Attempts to check the mail headers with the Relay Blackhole List. 
+Returns false if the headers check out fine or the query times out,
+returns a reason if the mail is considered spam.
+
 =item C<pipe($program)>
 
 This opens a pipe to an external program and feeds the mail to it.
@@ -207,6 +275,10 @@ This opens a pipe to an external program and feeds the mail to it.
 =item C<tidy>
 
 Tidies up the email as per L<Mail::Internet>
+
+=item C<get($header)>
+
+Retrieves the named header from the mail message.
 
 =item C<resent($address)>
 
